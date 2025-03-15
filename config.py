f7c8dcfefba6934beba00837326b3afff9d55b71
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2025-03-15 17:56:45 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,51 @@
+"""Titre du statut Bot"""
+Titre = "Gère l'économie du serveur"
+
+"""Etat du statut Bot"""
+Etat = "Créer par Bryan."
+
+"""Nom du role de gérant du Bot"""
+Gerant = "Gérant BoukiBot"
+
+"""Nom du role des mafieux du Bot"""
+Mafieux = "Mafieux"
+
+"""Nom du role de la banque de l'état du Bot"""
+Banque = "Banque"
+
+"""Préfix des commandes du Bot"""
+BOT_PREFIX = "+"
+
+"""Nom du Bot"""
+nom_bot = "Bot Personnalisé"
+
+"""Monnaie du Bot"""
+# peut-être personnalisée comme par exemple :
+# <:Bouken:1318277611612278794>
+monnaie = "🪙"
+
+"""Id du role de ping annonces du Bot"""
+ping_annonces_bot = 102347612345678901
+
+"""Token du Bot"""
+# A NE SURTOUT PAS PARTAGER
+# Exemple fictif
+token = "ODk4NzYwNzU4MTIzNDV6Y2VydGFpbl9leGFtcGxlX2JvdF9kZXZfYmFzZXBvc3RfdG9rZW4="
+
+"""Emoji du Bot pour les commandes réussies"""
+# peut-être personnalisée comme par exemple :
+# <:check_mark:1320445694305370165>
+emoji_worked = "✅"
+
+"""Emoji du Bot pour les commandes échouées"""
+# peut-être personnalisée comme par exemple :
+# <:cross_mark:1320445683936919613>
+emoji_error = "❌"
+
+"""Id du channel d'annonce du Bot"""
+# Exemple fictif
+channelBot = 102347612345678901
+
+"""Id du channel de logs du Bot"""
+# Exemple fictif
+log_channel = 102347612345678901
